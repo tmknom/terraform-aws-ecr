cb927dc4cac1dfda3c28381d232476788f438838
tmknom <tmk.nom@gmail.com>
2018-11-23 20:26:55 +0900

    Implement Policy Document of Only Pull

--- main.tf
+++ main.tf
@@ -6,3 +6,30 @@
 resource "aws_ecr_repository" "default" {
   name = "${var.name}"
 }
+
+# Allows specific accounts to pull images
+data "aws_iam_policy_document" "only_pull" {
+  statement {
+    sid    = "ElasticContainerRegistryOnlyPull"
+    effect = "Allow"
+
+    principals {
+      identifiers = ["${concat(list(local.current_account), local.only_pull_accounts)}"]
+      type        = "AWS"
+    }
+
+    # https://docs.aws.amazon.com/AmazonECR/latest/userguide/RepositoryPolicyExamples.html#IAM_allow_other_accounts
+    actions = [
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+    ]
+  }
+}
+
+locals {
+  only_pull_accounts = "${formatlist("arn:aws:iam::%s:root", var.only_pull_accounts)}"
+  current_account    = "${format("arn:aws:iam::%s:root", data.aws_caller_identity.current.account_id)}"
+}
+
+data "aws_caller_identity" "current" {}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "name" {
   type        = "string"
   description = "Name of the repository."
 }
+
+variable "only_pull_accounts" {
+  default     = []
+  type        = "list"
+  description = "AWS accounts which pull only."
+}
