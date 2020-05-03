cea902f0b8349d89c73ef7530f47f9fdd7c8681c
tmknom <tmk.nom@gmail.com>
2020-05-03 20:12:33 +0900

    Upgraded module to support Terraform 0.12

--- main.tf
+++ main.tf
@@ -4,13 +4,13 @@
 
 # https://www.terraform.io/docs/providers/aws/r/ecr_repository.html
 resource "aws_ecr_repository" "default" {
-  name = "${var.name}"
+  name = var.name
 }
 
 # https://www.terraform.io/docs/providers/aws/r/ecr_repository_policy.html
 resource "aws_ecr_repository_policy" "default" {
-  repository = "${aws_ecr_repository.default.name}"
-  policy     = "${data.aws_iam_policy_document.push_and_pull.json}"
+  repository = aws_ecr_repository.default.name
+  policy     = data.aws_iam_policy_document.push_and_pull.json
 }
 
 # Allows specific accounts to pull images
@@ -20,7 +20,15 @@ data "aws_iam_policy_document" "only_pull" {
     effect = "Allow"
 
     principals {
-      identifiers = ["${concat(list(local.current_account), local.only_pull_accounts)}"]
+      # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+      # force an interpolation expression to be interpreted as a list by wrapping it
+      # in an extra set of list brackets. That form was supported for compatibility in
+      # v0.11, but is no longer supported in Terraform v0.12.
+      #
+      # If the expression in the following list itself returns a list, remove the
+      # brackets to avoid interpretation as a list of lists. If the expression
+      # returns a single list item then leave it as-is and remove this TODO comment.
+      identifiers = [concat([local.current_account], local.only_pull_accounts)]
       type        = "AWS"
     }
 
@@ -36,14 +44,22 @@ data "aws_iam_policy_document" "only_pull" {
 # Allows specific accounts to push and pull images
 data "aws_iam_policy_document" "push_and_pull" {
   # An IAM policy document to import as a base for the current policy document
-  source_json = "${data.aws_iam_policy_document.only_pull.json}"
+  source_json = data.aws_iam_policy_document.only_pull.json
 
   statement {
     sid    = "ElasticContainerRegistryPushAndPull"
     effect = "Allow"
 
     principals {
-      identifiers = ["${concat(list(local.current_account), local.push_and_pull_accounts)}"]
+      # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+      # force an interpolation expression to be interpreted as a list by wrapping it
+      # in an extra set of list brackets. That form was supported for compatibility in
+      # v0.11, but is no longer supported in Terraform v0.12.
+      #
+      # If the expression in the following list itself returns a list, remove the
+      # brackets to avoid interpretation as a list of lists. If the expression
+      # returns a single list item then leave it as-is and remove this TODO comment.
+      identifiers = [concat([local.current_account], local.push_and_pull_accounts)]
       type        = "AWS"
     }
 
@@ -62,24 +78,24 @@ data "aws_iam_policy_document" "push_and_pull" {
 
 # https://www.terraform.io/docs/providers/aws/r/ecr_lifecycle_policy.html
 resource "aws_ecr_lifecycle_policy" "default" {
-  repository = "${aws_ecr_repository.default.name}"
-  policy     = "${data.template_file.ecr_lifecycle_policy.rendered}"
+  repository = aws_ecr_repository.default.name
+  policy     = data.template_file.ecr_lifecycle_policy.rendered
 }
 
 data "template_file" "ecr_lifecycle_policy" {
-  template = "${file("${path.module}/ecr_lifecycle_policy.json")}"
+  template = file("${path.module}/ecr_lifecycle_policy.json")
 
-  vars {
-    max_untagged_image_count = "${var.max_untagged_image_count}"
-    max_tagged_image_count   = "${var.max_tagged_image_count}"
-    tag_prefix_list          = "${jsonencode(var.tag_prefix_list)}"
+  vars = {
+    max_untagged_image_count = var.max_untagged_image_count
+    max_tagged_image_count   = var.max_tagged_image_count
+    tag_prefix_list          = jsonencode(var.tag_prefix_list)
   }
 }
 
 locals {
-  only_pull_accounts     = "${formatlist("arn:aws:iam::%s:root", var.only_pull_accounts)}"
-  push_and_pull_accounts = "${formatlist("arn:aws:iam::%s:root", var.push_and_pull_accounts)}"
-  current_account        = "${format("arn:aws:iam::%s:root", data.aws_caller_identity.current.account_id)}"
+  only_pull_accounts     = formatlist("arn:aws:iam::%s:root", var.only_pull_accounts)
+  push_and_pull_accounts = formatlist("arn:aws:iam::%s:root", var.push_and_pull_accounts)
+  current_account        = format("arn:aws:iam::%s:root", data.aws_caller_identity.current.account_id)
 }
 
 data "aws_caller_identity" "current" {}
