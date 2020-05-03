557051599d42c1082df03dfdea52ac7dc69fcd97
tmknom <tmk.nom@gmail.com>
2020-05-03 20:15:45 +0900

    Remove the brackets

--- main.tf
+++ main.tf
@@ -20,15 +20,7 @@ data "aws_iam_policy_document" "only_pull" {
     effect = "Allow"
 
     principals {
-      # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-      # force an interpolation expression to be interpreted as a list by wrapping it
-      # in an extra set of list brackets. That form was supported for compatibility in
-      # v0.11, but is no longer supported in Terraform v0.12.
-      #
-      # If the expression in the following list itself returns a list, remove the
-      # brackets to avoid interpretation as a list of lists. If the expression
-      # returns a single list item then leave it as-is and remove this TODO comment.
-      identifiers = [concat([local.current_account], local.only_pull_accounts)]
+      identifiers = concat([local.current_account], local.only_pull_accounts)
       type        = "AWS"
     }
 
@@ -51,15 +43,7 @@ data "aws_iam_policy_document" "push_and_pull" {
     effect = "Allow"
 
     principals {
-      # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-      # force an interpolation expression to be interpreted as a list by wrapping it
-      # in an extra set of list brackets. That form was supported for compatibility in
-      # v0.11, but is no longer supported in Terraform v0.12.
-      #
-      # If the expression in the following list itself returns a list, remove the
-      # brackets to avoid interpretation as a list of lists. If the expression
-      # returns a single list item then leave it as-is and remove this TODO comment.
-      identifiers = [concat([local.current_account], local.push_and_pull_accounts)]
+      identifiers = concat([local.current_account], local.push_and_pull_accounts)
       type        = "AWS"
     }
 
