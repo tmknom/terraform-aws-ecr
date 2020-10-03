aa08cb7d9a8e89fd12219fc15d5664f6313ab65f
tmknom <tmk.nom@gmail.com>
2020-10-03 22:24:36 +0900

    Convert resource name to this

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "ecr_repository_arn" {
-  value       = aws_ecr_repository.default.arn
+  value       = aws_ecr_repository.this.arn
   description = "Full ARN of the repository."
 }
 
 output "ecr_repository_name" {
-  value       = aws_ecr_repository.default.name
+  value       = aws_ecr_repository.this.name
   description = "The name of the repository."
 }
 
 output "ecr_repository_registry_id" {
-  value       = aws_ecr_repository.default.registry_id
+  value       = aws_ecr_repository.this.registry_id
   description = "The registry ID where the repository was created."
 }
 
 output "ecr_repository_url" {
-  value       = aws_ecr_repository.default.repository_url
+  value       = aws_ecr_repository.this.repository_url
   description = "The URL of the repository (in the form aws_account_id.dkr.ecr.region.amazonaws.com/repositoryName)"
 }
