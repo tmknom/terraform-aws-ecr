409516351cba8cf769172c564efa8b4f1e115c3a
tmknom <tmk.nom@gmail.com>
2020-10-18 20:10:43 +0900

    Upgraded examples/complete to support 'image_tag_mutability'

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -3,6 +3,7 @@ module "ecr" {
   name            = "example"
   tag_prefix_list = ["release"]
   scan_on_push    = true
+  image_tag_mutability = "IMMUTABLE"
 
   # If invalid account such as "123456789012" is specified, then cause error with following message.
   # Invalid parameter at 'PolicyText' failed to satisfy constraint: 'Invalid repository policy provided'
